f7f817c9076ceb90154c487e8d4caece85217c86
Maxime Livrieri <maxli57190@gmail.com>
2026-08-03 00:32:24 +0200

    fix: config.js remove aiprovider

--- apps/web/public/config.js
+++ apps/web/public/config.js
@@ -1,5 +1,4 @@
-window.__ODYSSEE_CONFIG__ = window.__ODYSSEE_CONFIG__ || {
+﻿window.__ODYSSEE_CONFIG__ = window.__ODYSSEE_CONFIG__ || {
   supabaseUrl: 'https://ziyzuklowskkcigrqdib.supabase.co',
   supabaseAnonKey: 'sb_publishable_Qgy6toD3eeu8jGTKDE66hQ_jpIBCMus',
-  aiProvider: 'mock',
 };
