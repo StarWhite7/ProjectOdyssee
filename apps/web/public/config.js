fbe89218607db3f0f629c04a97214f5b5b53da1b
Maxime Livrieri <maxli57190@gmail.com>
2026-08-02 23:59:12 +0200

    modif supabase

--- apps/web/public/config.js
+++ apps/web/public/config.js
@@ -1,5 +1,5 @@
 window.__ODYSSEE_CONFIG__ = window.__ODYSSEE_CONFIG__ || {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://ziyzuklowskkcigrqdib.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Qgy6toD3eeu8jGTKDE66hQ_jpIBCMus',
   aiProvider: 'mock',
 };
